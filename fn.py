9d0a2ca418b7c5390e21d8535b20d2d0bcd2c673
EscoBach <escobarmikael@hotmail.com>
2020-03-23 14:52:16 +0100

    new

--- fn.py
+++ fn.py
@@ -7,7 +7,6 @@ from scipy.stats import expon
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 #############################################
 # Kernel function for regression estimation #
@@ -53,12 +52,12 @@ def gen_data(nb_iter,size, a0, a1, a2, b0, b1, b2):
 # Generalized Beran estimator for the conditional survival function #
 #####################################################################
 def gene_Beran(t, obs, p, x=None, x_eval=None, h=1, mode_km=False):
-
     n = len(obs)
     csf = []
 
     #Compute the list of weights
     W = np.zeros(n)
+    W2 = np.zeros(n)
    
     if(mode_km):
         W = 1/(1+np.arange(n))
@@ -66,34 +65,24 @@ def gene_Beran(t, obs, p, x=None, x_eval=None, h=1, mode_km=False):
         W = quad_kern((x_eval-x)/h)
     
     W = W/np.sum(W)
-
     #Compute the list of the cumulative weights for the product
-    cum_W = (1-W/np.append([1],1-np.cumsum(W[np.arange(1,n)])))**p
+    cum_W = 1-np.append([0],np.cumsum(W[np.arange(0,n-1)]))
+    
+    W2[cum_W>0] = 1-W[cum_W>0]/cum_W[cum_W>0]
+    W2[cum_W<=0] = np.zeros(n)[cum_W<=0]
+
+    W2 = np.sign(W2)*np.abs(W2)**p
 
     # Compute the conditional survival function
-    for i in range(len(t)):
-    	csf.append(np.prod(cum_W[obs<=t[i]]))
+    for t_eval in t:
+        s = 1-np.prod(W2[obs<=t_eval])
+        csf.append(s)
 
     return csf
 
 ########################
 # Neural Network model #
 ########################
-# class Net(nn.Module):
-#
-#     def __init__(self,layers_size):
-#         super(Net, self).__init__()
-#         self.layers_size = layers_size
-#         self.hidden = nn.ModuleList([nn.Linear(layers_size[i], layers_size[i+1]) for i in range(len(layers_size)-1)])
-#
-#     def forward(self, x):
-#         if x.dim() == 0 :
-#             x = torch.tensor(x.item()*np.ones(self.layers_size[0]),device=x.device)
-#         for hidden_layer in self.hidden:
-#             x = F.relu(hidden_layer(x))
-#         return x
-
-
 class Net(nn.Module):
 
     def __init__(self,layers_size):
@@ -101,26 +90,25 @@ class Net(nn.Module):
         self.layers_size = layers_size
         layers = []
 
-
         for i in range(len(layers_size) - 1):
             layers.append(nn.Linear(layers_size[i], layers_size[i+1]))
             if(i != len(layers_size) - 2):
-                layers.append(nn.LeakyReLU(.2))
-
+                layers.append(nn.ReLU())
+            else:
+                layers.append(nn.Sigmoid())
 
         self.layers = nn.Sequential(*layers)
 
-
-
     def forward(self, x):
-
-        # if x.dim() == 0 :
-        #     x = torch.tensor(x.item()*np.ones(self.layers_size[0]),device=x.device)
-        # for hidden_layer in self.hidden:
-        #     x = F.relu(hidden_layer(x))
         return self.layers(x)
 
     def reset_parameters(self):
         for layer in self.layers:
             if hasattr(layer, 'reset_parameters'):
                 layer.reset_parameters()
+
+#def criterion(output,target):
+    #((target-output)**2).mean().item(),2*(target*(target-output)).mean().item()
+    #return torch.max(((target-output)**2).mean(),2*(target*(1-output)).mean())
+    #return torch.max(((output-target)**2).mean(),(target*(2*output-1)).mean())
+    #return torch.abs((output**2).mean() - target.mean())
--- main.py
+++ main.py
@@ -1,74 +1,127 @@
 from fn import *
 from tqdm import tqdm
+from os import system
+import matplotlib.pyplot as plt
+import math
 
 ##########
 #  Main  #
 ##########
-
-sample_size = 200
+sample_size = 1000
 nb_iter = 100
+nb_epoch = 200
 
 error_threshold = 0.01
 
-layers_size=[2,10,1]
+layers_size = [2,1000,1]
+x_list = [.3,.5,.7]
 
-a0 = 1
-a1 = 0
-a2 = 0
+a0 = 3
+a1 = .5
+a2 = .7
 
 b0 = 1
-b1 = 0
-b2 = 0
+b1 = .5
+b2 = .4
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #device="cpu"
 
 surv, censor, obs, delta,  xi, x = gen_data(nb_iter,sample_size, a0, a1, a2, b0, b1, b2)
 
+p = np.zeros((nb_iter,sample_size))
+###################
+# Neural Network  #
+###################
+print('Neural network training : sample size =',sample_size,'iteration =',nb_iter,'epoch =',nb_epoch)
 net = Net(layers_size)
 # net.to(torch.float)
 net.to(device)
 
-optimizer = torch.optim.Adam(net.parameters(),lr=.001)
-
-criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(net.parameters(),lr=.001)
+criterion = nn.L1Loss()
 
 #inputs = torch.tensor([obs,x],dtype=torch.double,device=device)
 #target = torch.tensor(delta,dtype=torch.double,device=device)
 outputs_batch = torch.ones(sample_size,dtype=torch.float,device=device)
 
+pbar = tqdm(range(nb_iter))
 
-nb_epoch = 1000000
-
-for k in range(nb_iter):
-	error = 1000
-	loop_c = 0
+for k in pbar:
 
 	inputs_batch = torch.transpose(torch.tensor([obs[k,], x[k,]],dtype=torch.float,device=device),0,1)
 	target_batch = torch.tensor(delta[k,],dtype=torch.float,device=device)
 
 	target_batch = target_batch.view(-1,1)
 
-	pbar = tqdm(range(nb_epoch))
-
 	net.reset_parameters()
 
-	for epoch in pbar:
+	for epoch in range(nb_epoch):
 		
-		loop_c += 1
-
-
 		outputs_batch = net(inputs_batch)
 
-
 		loss = criterion(outputs_batch,target_batch)
-
-		error = loss.item()
 
 		loss.backward()
 
 		optimizer.step()
 		optimizer.zero_grad()
 
+	p[k,:] = outputs_batch.cpu().data.numpy()[:,0]
 
-		pbar.set_postfix(loss=loss.item())
+	side = ((target_batch-outputs_batch)**2).mean().item(),2*(target_batch*(target_batch-outputs_batch)).mean().item()
+
+	pbar.set_postfix(loss=loss.item())
+
+#######################
+# Survival estimation #
+#######################
+print('Survival estimators computing')
+
+t = np.linspace(np.amin(surv),np.amax(surv),num=100)
+beran = np.zeros((nb_iter,len(t),len(x_list)))
+nn_beran = np.zeros((nb_iter,len(t),len(x_list)))
+
+for k in pbar:
+
+	#Bandwidth selection
+	h = .1
+	c_x = 0
+	for x_eval in x_list:
+		#Estimators computation
+		beran[k,:,c_x] = gene_Beran(t,surv[k,:],delta[k,:],x[k,:],x_eval,h)
+		nn_beran[k,:,c_x] = gene_Beran(t,surv[k,:],p[k,:],x[k,:],x_eval,h)
+
+		c_x += 1
+
+np.save('save/beran',beran)
+np.save('save/nn_beran',nn_beran)
+
+################
+# Plot results #
+################
+plt.figure()
+
+for i in range(len(x_list)):
+
+	true_cdf = expon(scale=1/(a0+a1*x_list[i]+a2*x_list[i]**2)).cdf(t)
+
+	mean_beran = np.mean(beran[:,:,i],axis=0)
+	mean_nn_beran = np.mean(nn_beran[:,:,i],axis=0)
+
+	mise_beran = np.mean((beran[:,:,i]-true_cdf)**2,axis=0)
+	mise_nn_beran = np.mean((nn_beran[:,:,i]-true_cdf)**2,axis=0)
+
+	plt.subplot(len(x_list),2,2*i+1)
+	plt.plot(t,mean_beran,label='Beran')
+	plt.plot(t,mean_nn_beran,label='NN Beran')
+	plt.plot(t,true_cdf,label='cdf')
+
+	plt.legend()
+
+	plt.subplot(len(x_list),2,2*(i+1))
+	plt.plot(t,mise_beran,label='Beran')
+	plt.plot(t,mise_nn_beran,label='NN Beran')
+
+	plt.legend()
+plt.show()
